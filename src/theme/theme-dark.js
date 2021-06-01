eb18807f30fed9d83563cc98ff77223bb68c46cb
Paulo Colombo <paulo.colombo@pm.me>
2021-06-01 22:34:38 +0200

    Revert "Info box text color changed"

--- src/theme/theme-dark.js
+++ src/theme/theme-dark.js
@@ -30,7 +30,7 @@ export default {
 
   info: '#00d2ff',
   infoSurface: '#6F82F5',
-  infoSurfaceContent: '#fffffff',
+  infoSurfaceContent: '#B7C0FA',
 
   help: '#7C80F2',
   helpContent: '#FFFFFF',
